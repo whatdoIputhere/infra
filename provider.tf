c3a84470565eff7fae764c9963cbe46e27781c00
pecarmo_deloitte <pecarmo@deloitte.pt>
2024-06-13 11:29:33 +0100

    fix backend rg name

--- provider.tf
+++ provider.tf
@@ -5,7 +5,7 @@ provider "azurerm" {
 
 terraform {
   backend "azurerm" {
-    resource_group_name   = "statestoragerg"
+    resource_group_name   = "statestoragerg19910"
     storage_account_name  = "statestacc19910"
     container_name        = "statestoragecontainer"
     key                   = "terraform.tfstate"
