2941fe9dcf532d47253313ee2e0c21acc22844e8
whatdoIputhere <pedromgc21@gmail.com>
2024-06-13 15:38:38 +0100

    change aks version

--- main.tf
+++ main.tf
@@ -54,7 +54,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
     location            = azurerm_resource_group.rg.location
     resource_group_name = azurerm_resource_group.rg.name
     dns_prefix          = "pecarmoaks"
-    kubernetes_version  = "1.19.7"
+    kubernetes_version  = "1.27"
 
     default_node_pool {
         name       = "default"
