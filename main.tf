1c4a32d22cf9b4946c99823b9366617cbca56aa6
whatdoIputhere <pedromgc21@gmail.com>
2024-06-13 15:21:27 +0100

    add access policy

--- main.tf
+++ main.tf
@@ -20,3 +20,17 @@ resource "azurerm_key_vault" "keyvault" {
     tenant_id = data.azurerm_client_config.current.tenant_id
     sku_name            = "standard"
 }
+
+resource "azurerm_key_vault_access_policy" "keyvaultpolicypecarmo" {
+    key_vault_id = azurerm_key_vault.keyvault.id
+
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = "bffd9bac-f216-407d-9dac-e328b3944ef0"
+
+    secret_permissions = [
+        "get",
+        "list",
+        "set",
+        "delete",
+    ] 
+}
