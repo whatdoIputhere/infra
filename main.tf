5bf4a1b433902e98faffd1432039e0fbdff085ca
whatdoIputhere <pedromgc21@gmail.com>
2024-06-13 15:17:05 +0100

    add keyvault

--- main.tf
+++ main.tf
@@ -1,3 +1,4 @@
+data "azurerm_client_config" "current" {}
 resource "azurerm_resource_group" "rg" {
     name     = "infra-rg-neu"
     location = "North Europe"
@@ -12,30 +13,10 @@ resource "azurerm_container_registry" "registry" {
     public_network_access_enabled = true
 }
 
-resource "azurerm_kubernetes_cluster" "aks" {
-    name                = "pecarmoaks"
-    location            = azurerm_resource_group.rg.location
+resource "azurerm_key_vault" "keyvault" {
+    name                = "pecarmokeyvault"
     resource_group_name = azurerm_resource_group.rg.name
-    dns_prefix          = "pecarmoaks"
-    kubernetes_version  = "1.19.7"
-
-    default_node_pool {
-        name       = "default"
-        node_count = 1
-        vm_size    = "Standard_DS2_v2"
-    }
-
-    identity {
-        type = "SystemAssigned"
-    }
-
-    network_profile {
-        network_plugin = "azure"
-    }
-
-    service_principal {
-        client_id     = data.azurerm_client_config.current.client_id
-        client_secret = data.azurerm_client_config.current.client_secret
-    }
+    location            = azurerm_resource_group.rg.location
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    sku_name            = "standard"
 }
-data "azurerm_client_config" "current" {}
