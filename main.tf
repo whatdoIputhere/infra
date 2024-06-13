79414f0fe2ca4251d2db1128f30cbfdbf58567ab
whatdoIputhere <pedromgc21@gmail.com>
2024-06-13 15:05:45 +0100

    add aks cluster

--- main.tf
+++ main.tf
@@ -11,3 +11,30 @@ resource "azurerm_container_registry" "registry" {
     admin_enabled             = true
     public_network_access_enabled = true
 }
+
+resource "azurerm_kubernetes_cluster" "aks" {
+    name                = "pecarmoaks"
+    location            = azurerm_resource_group.rg.location
+    resource_group_name = azurerm_resource_group.rg.name
+    dns_prefix          = "pecarmoaks"
+    kubernetes_version  = "1.19.7"
+
+    default_node_pool {
+        name       = "default"
+        node_count = 1
+        vm_size    = "Standard_DS2_v2"
+    }
+
+    identity {
+        type = "SystemAssigned"
+    }
+
+    network_profile {
+        network_plugin = "azure"
+    }
+
+    service_principal {
+        client_id     = data.azurerm_client_config.current.client_id
+        client_secret = data.azurerm_client_config.current.client_secret
+    }
+}
