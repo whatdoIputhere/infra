236206192cde96ec7c4497322cb86753a0b1a22d
whatdoIputhere <pedromgc21@gmail.com>
2024-06-20 23:52:22 +0100

    fix change vm size

--- main.tf
+++ main.tf
@@ -56,7 +56,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
     default_node_pool {
         name       = "default"
         node_count = 1
-        vm_size    = "Standard_B1s"
+        vm_size    = "Standard_B2ms"
 
         upgrade_settings {
             drain_timeout_in_minutes      = 0
