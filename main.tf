a4db123eaeb8f16c3446396b934144e62456be2c
whatdoIputhere <pedromgc21@gmail.com>
2024-07-09 17:36:23 +0100

    change vm size to b2s

--- main.tf
+++ main.tf
@@ -56,7 +56,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
     default_node_pool {
         name       = "default"
         node_count = 1
-        vm_size    = "Standard_B2ms"
+        vm_size    = "Standard_B2s"
         os_sku = "Ubuntu"
         os_disk_size_gb = "32"
 
